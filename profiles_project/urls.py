60f5439c30f937093699ae2170105507e67f5bad
muriloms <ms7murilo@gmail.com>
2021-05-05 14:49:33 -0300

    Added login API

--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+from profiles_api import views 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.UserLoginApiView.as_view()),
     path('api/', include('profiles_api.urls')),
 ]
